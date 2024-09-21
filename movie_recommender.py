9a74e1f6d41f9aab6ff810c1bcab35a386b3b0db
Xianghan Mei <xianghan.mei@hec.edu>
2024-09-21 17:47:02 +0200

    Updated texts in the app

--- movie_recommender.py
+++ movie_recommender.py
@@ -2,10 +2,12 @@ import streamlit as st
 from utils.utils import load_data, load_model_and_recommend, get_movie_name, get_filtered_movies
 
 if __name__ == "__main__":
+    # Load the data
     file_name = 'ml-100k'
     file_path = ''.join(['./', file_name])
     data = load_data()
 
+    # Construct the mapping dictionaries between movie names and raw ids
     movie_id_to_name = {}
     movie_name_to_id = {}
 
@@ -17,23 +19,24 @@ if __name__ == "__main__":
             movie_id_to_name[movie_id] = movie_title
             movie_name_to_id[movie_title.lower()] = movie_id
 
+    # Extract movies used in the training process
     model_path = './model/knn_movie_recommender.model'
     filtered_movies = get_filtered_movies(model_path, movie_id_to_name)
 
-    # Streamlit App
-    st.title("Movie Recommender System")
+    # Streamlit App title
+    st.title("Movie Recommender")
 
-    # Pulling down menu to select a movie
+    # Set up a drop-down menu for movie selection to avoid movie name mismatch
     movie_options = filtered_movies
     selected_movie_name = st.selectbox(
         "Select a movie you like:",
         options=movie_options
     )
 
-    # Button to trigger recommendation
+    # Set up a button to trigger recommendation
     if st.button("Recommend"):
         recommendation = load_model_and_recommend(selected_movie_name, 
                                                 model_path, 
                                                 movie_name_to_id=movie_name_to_id, 
                                                 movie_id_to_name=movie_id_to_name)
-        st.write(f"If you liked '{selected_movie_name}', you might also like '{recommendation}'.")
+        st.write(f"If you liked '{selected_movie_name}', you might also like '{recommendation}' according to our prediction.")
